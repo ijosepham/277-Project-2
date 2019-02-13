5654b6b6452d3ef4f8c5cc8b289bf1bb6515ab60
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-12 22:35:14 -0800

    updated macaron class with constructors, tostring, and cost

--- 277 Project 2/src/Macaron.java
+++ 277 Project 2/src/Macaron.java
@@ -1,4 +1,33 @@
 
-public class Macaron {
+public class Macaron extends DessertItem {
+	private int quantity;
+	private double price = 1.00;
+	private double trioPrice = 2.50;
+	
+	public Macaron ( ) {
+		quantity = 1;
+		price = 1.25;
+	}
+	
+	public Macaron ( int q ) {
+		quantity = q;
+	}
+	
+	public String toString ( ) {
+		if ( quantity > 1 ) {
+			return quantity + " Macarons";
+		} else {
+			return quantity + " Macaron";
+		}
+	}
+	
+	@Override
+	public double getCost() {
+		double cost = 0;
+		int threes = quantity / 3;
+		int singles = quantity % 3;
+		cost = singles * price + threes * trioPrice;
+		return cost;
+	}
 
 }
