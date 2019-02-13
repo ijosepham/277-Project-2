70d3ff55245ae981f2f817cd8caa8eff9b19635d
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-12 22:27:39 -0800

    created pastry constructors, tostring, and cost

--- 277 Project 2/src/Pastry.java
+++ 277 Project 2/src/Pastry.java
@@ -1,4 +1,31 @@
 
-public class Pastry {
+public class Pastry extends DessertItem {
+	private int quantity;
+	private int price = 1;
+	private String temperature;
+	
+	public Pastry ( ) {
+		quantity = 1;
+		temperature = "Warm";
+	}
+	
+	public Pastry ( int q, String t ) {
+		quantity = q;
+		temperature = t;
+	}
+	
+	public String toString ( ) {
+		if ( quantity > 1 ) {
+			return quantity + " " + temperature + " Pastries";
+		} else {
+			return "1 " + temperature + " Pastry";
+		}
+		
+	}
+	
+	@Override
+	public double getCost() {
+		return quantity * price;
+	}
 
 }
