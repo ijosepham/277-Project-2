d8ba95e708ca9288920023edd05139113b2d5c54
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-12 19:56:20 -0800

    create class with constructors and toString

--- 277 Project 2/src/BobaDrink.java
+++ 277 Project 2/src/BobaDrink.java
@@ -1,4 +1,27 @@
 
-public class BobaDrink {
+public class BobaDrink extends DrinkItem {
+	private String topping;
+	private String milk;
+	
+	public BobaDrink ( ) {
+		super ( );
+		topping = "";
+		milk = "";
+	}
+	
+	public BobaDrink ( String n, String sw, String m, String t, String sz ) {
+		super ( n, sw, sz );
+		topping = t;
+		milk = m;
+	}
+	
+	public String toString ( ) {
+		return super.toString ( ) + " with " + topping + " and " + milk;
+	}
+	
+	@Override
+	public double getCost() {
+		return 0;
+	}
 
 }
