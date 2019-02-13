9ab01db04a971043b81224355074efb16f48582d
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-12 16:52:20 -0800

    create constructors, setters, getters, and toString for desserts and drinks

--- 277 Project 2/src/DessertItem.java
+++ 277 Project 2/src/DessertItem.java
@@ -1,4 +1,28 @@
 
-public class DessertItem {
+public abstract class DessertItem {
+	private String name;
+	
+	public DessertItem ( ) {
+		name = "";
+	}
+	
+	public DessertItem ( String n ) {
+		name = n;
+	}
+	
+	public void setName ( String n ) {
+		name = n;
+	}
+	
+	public String getName ( ) {
+		return name;
+	}
 
+	
+	public String toString ( ) {
+		String s = name;
+		return s;
+	}
+	
+	public abstract double getCost ( );
 }
--- 277 Project 2/src/DrinkItem.java
+++ 277 Project 2/src/DrinkItem.java
@@ -1,4 +1,49 @@
 
-public class DrinkItem {
+public abstract class DrinkItem {
+	private String name;
+	private int sweetness;
+	private String size;
+	
+	public DrinkItem ( ) {
+		name = "";
+		sweetness = 100;
+		size = "Medium";
+	}
+	
+	public DrinkItem ( String n, int sw, String sz ) {
+		name = n;
+		sweetness = sw;
+		size = sz;
+	}
+	
+	public void setName ( String n ) {
+		name = n;
+	}
+	
+	public void setSweetness ( int sw ) {
+		sweetness = sw;
+	}
+	
+	public void setSize ( String sz ) {
+		size = sz;
+	}
+	
+	public String getName ( ) {
+		return name;
+	}
+	
+	public int getSweetness ( ) {
+		return sweetness;
+	}
 
+	public String getSize ( ) {
+		return size;
+	}
+	
+	public String toString ( ) {
+		String s = size + " " + name +  ", " + sweetness + "% Sweetness.";
+		return s;
+	}
+	
+	public abstract double getCost ( );
 }
