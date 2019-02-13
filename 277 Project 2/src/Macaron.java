8a81f03a9c915d3e896c0987f5c17c534b66fa8b
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-12 22:46:24 -0800

    added some instance variables and updated getcost function

--- 277 Project 2/src/Macaron.java
+++ 277 Project 2/src/Macaron.java
@@ -1,32 +1,34 @@
 
 public class Macaron extends DessertItem {
 	private int quantity;
+	private int singles;
+	private int trios;
 	private double singlePrice = 1.00;
 	private double trioPrice = 2.50;
 	
 	public Macaron ( ) {
 		quantity = 1;
+		singles = 1;
+		trios = 0;
 	}
 	
 	public Macaron ( int q ) {
 		quantity = q;
+		singles = quantity % 3;
+		trios = quantity / 3;
 	}
 	
 	public String toString ( ) {
 		if ( quantity > 1 ) {
 			return quantity + " Macarons";
 		} else {
-			return quantity + " Macaron";
+			return "1 Macaron";
 		}
 	}
 	
 	@Override
 	public double getCost() {
-		double cost = 0;
-		int threes = quantity / 3;
-		int singles = quantity % 3;
-		cost = singles * singlePrice + threes * trioPrice;
-		return cost;
+		return singles * singlePrice + trios * trioPrice;
 	}
 
 }
