ef381f2653959e719aa9fd5ddb815610cc081da3
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-12 22:37:07 -0800

    updated variable name

--- 277 Project 2/src/Macaron.java
+++ 277 Project 2/src/Macaron.java
@@ -1,12 +1,11 @@
 
 public class Macaron extends DessertItem {
 	private int quantity;
-	private double price = 1.00;
+	private double singlePrice = 1.00;
 	private double trioPrice = 2.50;
 	
 	public Macaron ( ) {
 		quantity = 1;
-		price = 1.25;
 	}
 	
 	public Macaron ( int q ) {
@@ -26,7 +25,7 @@ public class Macaron extends DessertItem {
 		double cost = 0;
 		int threes = quantity / 3;
 		int singles = quantity % 3;
-		cost = singles * price + threes * trioPrice;
+		cost = singles * singlePrice + threes * trioPrice;
 		return cost;
 	}
 
