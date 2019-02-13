7995ca55da2b96ccd8e4b70dc5c123889f07aa19
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-12 20:18:31 -0800

    update coffee class with consturctors and a toString

--- 277 Project 2/src/CoffeeDrink.java
+++ 277 Project 2/src/CoffeeDrink.java
@@ -1,4 +1,23 @@
 
-public class CoffeeDrink {
+public class CoffeeDrink extends DrinkItem {
+	private String base;
+	
+	public CoffeeDrink ( ) {
+		super ( );
+	}
+	
+	public CoffeeDrink ( String sw, String sz, String b ) {
+		super ( "Coffee", sw, sz );
+		base = b;
+	}
+	
+	public String toString ( ) {
+		return super.getSize ( ) + " " + base + " based Coffee with " + super.getSweetness ( ) + " of Sugar";
+	}
+	
+	@Override
+	public double getCost() {
+		return 0;
+	}
 
 }
