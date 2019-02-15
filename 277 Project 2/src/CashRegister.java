83fa3675b6e7fb22668ee5d3fe30c9145682b640
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-15 13:46:46 -0800

    extends array list, optinos to print receip, calculate balance,a dn celar register

--- 277 Project 2/src/CashRegister.java
+++ 277 Project 2/src/CashRegister.java
@@ -1,4 +1,60 @@
+import java.util.ArrayList;
 
-public class CashRegister {
+public class CashRegister extends ArrayList < MenuItem > {
 	
+	public CashRegister ( ) {
+		super ( );
+	}
+	
+	public int orderSize ( ) {
+		return super.size ( );
+	}
+	
+	public MenuItem addOrder ( MenuItem order ) {
+		super.add ( order );
+		return order;
+	}
+	
+	public MenuItem getOrder ( int index ) {
+		return super.get ( index );
+	}
+	
+	public MenuItem removeOrder ( ) {
+		return super.remove ( 0 );
+	}
+	
+	public MenuItem removeOrder ( int index ) {
+		return super.remove ( index );
+	}
+	
+	public MenuItem removeOrder ( MenuItem order ) {
+		super.remove ( order );
+		return order;
+	}
+	
+	public double calculateBalance ( ) {
+		int orderSize = orderSize ( );
+		double balance = 0;
+		
+		for ( int i = 0; i < orderSize; i ++ ) {
+			balance += getOrder ( i ).getCost ( );
+		}
+		
+		return balance;
+	}
+	
+	public void printReceipt ( ) {
+		int orderSize = orderSize ( );
+		System.out.println ( "Order Receipt" );
+		for ( int i = 0; i < orderSize; i ++ ) {
+			System.out.println ( getOrder ( i ) );
+		}
+	}
+	
+	public void clearRegister ( ) {
+		int orderSize = orderSize ( );
+		for ( int i = 0; i < orderSize; i ++ ) {
+			removeOrder ( );
+		}
+	}
 }
