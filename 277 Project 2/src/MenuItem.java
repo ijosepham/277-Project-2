a1232b586071dc815cf2889e33c84c601c66764a
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-15 16:49:53 -0800

    passes in quantity q

--- 277 Project 2/src/MenuItem.java
+++ 277 Project 2/src/MenuItem.java
@@ -1,6 +1,6 @@
 
 public abstract class MenuItem {
-	public abstract void setQuantity ( );
+	public abstract void setQuantity ( int q );
 	public abstract String getName ( );
 	public abstract double getCost ( );
 }
