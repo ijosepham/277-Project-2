1c10b90105e73b3e9d0ecc7293b1029c21fae389
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-21 03:12:14 -0800

    chagned a tostring

--- 277 Project 2/src/Pastry.java
+++ 277 Project 2/src/Pastry.java
@@ -25,12 +25,18 @@ public class Pastry extends DessertItem {
 	}
 	
 	public String toString ( ) {
-		String s = quantity + " x " + temperature + " " + super.getName ( );
+		String s = quantity + " x " + temperature + " " + super.getName ( ) + "\n";
 		return s;
 	}
 	
 	@Override
 	public double getCost ( ) {
 		return quantity * price;
+	}
+
+	@Override
+	public int compareTo(DessertItem o) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
