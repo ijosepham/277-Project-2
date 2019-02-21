040e30310b1287ae4afe8826f37c3a6075b58b66
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-21 03:14:41 -0800

    multiple toppings can nbow be chosen

--- 277 Project 2/src/BobaDrink.java
+++ 277 Project 2/src/BobaDrink.java
@@ -16,9 +16,36 @@ public class BobaDrink extends DrinkItem {
 		milk = m;
 	}
 	
-//	public String toString ( ) {
-//		return super.getSize ( ) + " " + topping + " " + super.getName( ) + " with " + milk;
-//	}
+	public void setToppings ( ArrayList < String > t ) {
+		toppings = t;
+	}
+	
+	public void setMilk ( String m ) {
+		milk = m;
+	}
+	
+	public ArrayList < String > getToppings ( ) {
+		return toppings;
+	}
+	
+	public String getMilk ( ) {
+		return milk;
+	}
+	
+	public String toString ( ) {
+		 String s = super.getSize ( ) + " " + super.getName ( ) + "\n";
+		 s += " -" + super.getSweetness ( ) + "\n";
+		 s += " -" + milk + "\n";
+		 if ( toppings.size ( ) == 0 ) {
+			 s += " -No Toppings" + "\n";
+		 } else {
+			 s += " -" + toppings.size ( ) + " Toppings" + "\n";
+			 for ( int i = 0; i < toppings.size ( ); i ++ ) {
+				 s += "   -" + toppings.get ( i ) + "\n";
+			 }
+		 }
+		 return s;
+	}
 	
 	@Override
 	public double getCost() {
@@ -32,5 +59,11 @@ public class BobaDrink extends DrinkItem {
 			cost += 4.00;
 		} 
 		return cost;
+	}
+
+	@Override
+	public int compareTo(DrinkItem o) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
