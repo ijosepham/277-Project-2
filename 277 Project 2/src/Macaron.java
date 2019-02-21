3dc9fc73e7ff110fe608fdb68e09f2d4f9b6a20c
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-21 03:12:07 -0800

    made instances of singles and threes for macarons because there's a discount if there're three of macarons

--- 277 Project 2/src/Macaron.java
+++ 277 Project 2/src/Macaron.java
@@ -1,17 +1,23 @@
 
 public class Macaron extends Cookie {
 	private int quantity;
+	private int singles;
+	private int threes;
 	private double pricePerOne = 1.00;
 	private double pricePerThree = 2.50;
 	
 	public Macaron ( ) {
 		super ( "Macaron", 1 );
 		quantity = 1;
+		singles = 1;
+		threes = 0;
 	}
 	
 	public Macaron ( String m, int q ) {
 		super ( m, q );
 		quantity = q;
+		singles = quantity % 3;
+		threes = quantity / 3;
 	}
 	
 	public int getQuantity ( ) {
@@ -20,25 +26,25 @@ public class Macaron extends Cookie {
 	
 	public void setQuantity ( int q ) {
 		quantity = q;
+		singles = quantity % 3;
+		threes = quantity / 3;
 	}
 	
 	public String toString ( ) {
-		int threes = quantity / 3;
-		int singles = quantity % 3;
+		String singlesPrice = String.format ( "%.2f", singles * pricePerOne );
+		String threesPrice = String.format ( "%.2f", threes * pricePerThree);
 		String s = "";
 		if ( threes > 0 ) {
-			s += threes + " x Three " + super.getName ( ) + "s" + "\n";
+			s += threes + " x Three " + super.getName ( ) + "s - $" + threesPrice + "\n";
 		}
 		if ( singles > 0 ) {
-			s += singles + " x " + super.getName ( ) + "\n";
+			s += singles + " x " + super.getName ( ) + " - $" + singlesPrice + "\n";
 		}
 		return s;
 	}
 	
 	@Override
 	public double getCost ( ) {
-		int threes = quantity / 3;
-		int singles = quantity % 3;
 		return singles * pricePerOne + threes * pricePerThree;
 	}
 }
