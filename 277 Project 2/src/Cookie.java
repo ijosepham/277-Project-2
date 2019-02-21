a715b0eed43f4dd90eb8e51c5f95291e5d0dba1d
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-02-21 03:12:43 -0800

    replaced quantity with dozens

--- 277 Project 2/src/Cookie.java
+++ 277 Project 2/src/Cookie.java
@@ -1,34 +1,39 @@
 
 public class Cookie extends DessertItem {
-	private int quantity;
+	private int dozens;
 	private double pricePerDozen = 10.00;
 	
 	public Cookie ( ) {
 		super ( "Cookie" );
-		quantity = 1;
+		dozens = 1;
 	}
 	
-	public Cookie ( String c, int q ) {
+	public Cookie ( String c, int d ) {
 		super ( c );
-		quantity = q;
+		dozens = d;
 	}
 	
 	public int getQuantity ( ) {
-		return quantity;
+		return dozens;
 	}
 	
-	public void setQuantity ( int q ) {
-		quantity = q;
+	public void setQuantity ( int d ) {
+		dozens = d;
 	}
 	
 	public String toString ( ) {
-		String s = quantity + " x Dozen of " + super.getName ( ) + "s";
+		String s = dozens + " x Dozen of " + super.getName ( ) + "s" + "\n";
 		return s;
 	}
 	
 	@Override
-	public double getCost() {
-		int dozens = quantity / 12;
+	public double getCost ( ) {
 		return dozens * pricePerDozen;
+	}
+
+	@Override
+	public int compareTo(DessertItem arg0) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
